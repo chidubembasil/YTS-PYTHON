d81e03e604891088b315f59db6772cf87c117200
BASIL CHIDUBEM <chidbasil@gmail.com>
2026-01-13 13:24:43 +0100

    done

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# items = ("apple", "banana", "cherry")
+# print(items)
+
+
+# print(items[0])
+# print(items[1])
+# print(items[2])
+
+# print(len(items))
+# bag = (1, "dog", True, 3.14)
+# print(bag)
+
+
+container = input("Enter an item: ")
+container_In = (container,)
+print(container_In)
+# print(container_In[0])
