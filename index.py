49b4ae95e312a85ae125378921a6e1cba91dc472
Adeola-cyber <peteradeyanju737@gmail.com>
2026-01-14 10:14:08 +0100

    done

---
+++ index.py
@@ -0,0 +1,52 @@
+# print("Simple Calculator")
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+
+# print("Addition:", num1 + num2)
+# print("Multiplication:", num1 * num2)
+# print("Division:", num1 / num2)
+
+
+
+# num1 = float(input("Enter first number"))
+# op = input("Enter operator (+,-,*,/)")
+# num2 = float(input("Enter second number"))
+
+
+# if op=="+":
+#     result = num1+num2
+# elif op == "-":
+#     result = num1+num2
+# elif op == "*":
+#     result = num1+num2
+# elif op == "/":
+#     if num2 ==0:
+
+# tuples example
+
+# items = ("apple", "bannana", "cherry")
+# print(items)
+
+# print(items[0])
+# print(items[1])
+# print(items[2])
+
+# print(len(items))
+# bag = (1, "dog", True, 3.14)
+# print(bag)
+
+
+food = input("Enter a food name").lower()
+
+if food == "rice":
+    print("Rice is available")
+elif food == "yam":
+    print("yam is available")
+elif food == "beans":
+    print("beans is available")
+elif food == "pizza":
+    print == ("pizza is available")       
+else:
+
+    print("Sorry that food is not available")        
